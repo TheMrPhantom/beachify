ac163175bc298c4e3372927374929420d96c902e
themrphantom <diemilbe@gmx.net>
2022-03-30 21:16:55 +0200

    added oauth2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import Cookies from 'js-cookie';
 import { themes } from './Components/Common/Theme';
-
+import OAuth2Login from 'react-simple-oauth2-login';
 import allReducer from './Reducer/reducerCombiner';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -21,12 +21,22 @@ function App() {
     setthemeCookie(Cookies.get("theme") !== undefined ? Number(Cookies.get("theme")) : 0)
   }, [])
 
+  const onSuccess = (response: any) => console.log(response);
+  const onFailure = (response: any) => console.error(response);
+
   return (
     <ThemeProvider theme={themes[themeCookie]}>
       <Router>
         <div className="App">
           <CssBaseline />
           <Provider store={store}>
+            <OAuth2Login
+              authorizationUrl="https://auth.stuvus.uni-stuttgart.de/oauth2/auth"
+              responseType="token"
+              clientId=""
+              redirectUri="https://beachify.fius.de/"
+              onSuccess={onSuccess}
+              onFailure={onFailure} />
             <div className={style.app}>
               <SongArea />
               <QueueArea />
